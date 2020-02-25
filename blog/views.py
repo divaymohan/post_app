6fe998a04fc6e1c0e4c449dfa17ff115886527a8
divaymohan <divaymohan05@gmail.com>
2020-02-25 12:47:39 +0530

    its starting of templats

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render()
+
+
+def about(request):
+    return HttpResponse('<h1>about page</h1>')
