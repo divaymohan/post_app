4678a1f6be9f7105501ff18591372af922f62deb
divaymohan <divaymohan05@gmail.com>
2020-03-06 18:04:09 +0530

    admin settings done and Post also registered to admin page

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'Divay Mohan',
-        'title': 'Worker 1',
-        'content': 'Worker Specification',
-        'date_posted': 'March 4 2019'
-    },
-    {
-        'author': 'Aashish Dahiya',
-        'title': 'Worker 2',
-        'content': 'Worker Specification',
-        'date_posted': 'March 4 2019'
-    }
-]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all(),
