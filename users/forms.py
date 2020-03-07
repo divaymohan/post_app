0f20260907f838a0b2807e4921b11528dabcaaa4
divaymohan <divaymohan05@gmail.com>
2020-03-07 21:49:20 +0530

    firstname and lastname successfully stored in database

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    full_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
 
     class Meta:
         model = User
-        fields = ['username', 'full_name', 'email',  'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name',
+                  'email',  'password1', 'password2']
