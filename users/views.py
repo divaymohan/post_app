ae5fa0e713108e2f07257c6cb07bf05aa184a2c6
divaymohan <divaymohan05@gmail.com>
2020-03-07 22:57:17 +0530

    logout functionality added and profile accessable only when user is logged in..git add

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.s
@@ -24,3 +25,8 @@ def register(request):
             }
 
     return render(request, 'users/register.html', form)
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
